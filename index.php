0d6af18c79b89dd8f1c10f097703cf9a7459e3fe
JAgarcia06 <nata006as@hotmail.com>
2017-05-06 10:23:01 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,12 +6,42 @@
 	<link rel="stylesheet" href="style.css" />
 </head>
 <body>
-	<div>
-		<img style="text-align:center;" src='images/inicio.jpg'>
+	<div style="text-align:center;">
+		<img src='images/inicio.jpg'>
 	</div>
 	
-	<div>
-		<img style="text-align:center;" src='images/final.jpg'>
+	<table style="text-align:center;">
+	   <?php
+	  $servername = "us-cdbr-iron-east-03.cleardb.net";
+	  $username = "b74ba3320e82ec";
+	  $password = "2d194843";
+	  $dbname = "ad_bb6ddb2f34daf52";
+	  // Create connection
+	  $conn = new mysqli($servername, $username, $password, $dbname);
+	  // Check connection
+	  if ($conn->connect_error) {
+	      die("Connection failed: " . $conn->connect_error);
+	  }
+	  $sql = "SELECT * from servicios";
+	  $result = $conn->query($sql);
+	  if ($result->num_rows > 0) {
+	      // output data of each row
+	      while($row = $result->fetch_assoc()) {?>
+	      <tr>
+	      <td><?php echo $row['nombre_servicio']?></td>
+	      <td><?php echo $row['descripcion_servicio']?></td>
+	      <img sr='images/'> <td><?php echo $row['imagen_servicio']?></td>
+	      </tr>
+	     <?php }
+	  } else {
+	      echo "0 results";
+	  }
+	  $conn->close();
+	 ?> 
+	 </table>
+	
+	<div style="text-align:center;" >
+		<img src='images/final.jpg'>
 	</div>	
 </body>
 </html>
